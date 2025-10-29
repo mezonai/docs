1abdc722fe83e1e186c4d36eb8491e941024b1f1
thang.thieuquang <thang.thieuquang@ncc.asia>
2025-10-29 13:59:59 +0700

    refactor: update dark theme color

--- src/components/IndexCard/index.tsx
+++ src/components/IndexCard/index.tsx
@@ -1,3 +1,5 @@
+import styles from './styles.module.css';
+
 export function IndexCard({
   href,
   title,
@@ -11,11 +13,11 @@ export function IndexCard({
 }) {
   return (
     <article className="col col--6" style={{ marginBottom: "2rem" }}>
-      <a className="card padding--lg card__container" href={href}>
-        <h2 className="text--truncate card__title">
+      <a className={`card padding--lg ${styles.card__container}`} href={href}>
+        <h2 className={`text--truncate ${styles.card__title}`}>
           {isFolder ? "🗃️" : "📄️"} {title}
         </h2>
-        <p className="text--truncate card__description">{description}</p>
+        <p className={`text--truncate ${styles.card__description}`}>{description}</p>
       </a>
     </article>
   );
