7327a7ead3558713a88350aa2dda8f1c301a632c
thang.thieuquang <thang.thieuquang@ncc.asia>
2025-10-30 09:50:50 +0700

    feat: add redirect locale by browser lang

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import type { ReactNode } from "react";
+import { useEffect } from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
 
-import styles from './index.module.css';
-import Translate from '@docusaurus/Translate';
+import styles from "./index.module.css";
+import Translate from "@docusaurus/Translate";
+import { useHistory, useLocation } from "@docusaurus/router";
+import { normalizePath } from "../utils/utils";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
@@ -21,7 +24,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/user/welcome">
+            to="/user/welcome"
+          >
             <Translate id="homepage.getStarted">Bắt đầu</Translate>
           </Link>
         </div>
@@ -31,11 +35,34 @@ function HomepageHeader() {
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    const browserLang = navigator.language?.split("-")[0];
+    const normalizedPath = normalizePath(location.pathname);
+    if (
+      (normalizedPath === "/docs/en" && browserLang === "en") ||
+      (normalizedPath === "/docs" && browserLang === "vi")
+    )
+      return;
+
+    const supported = ["vi", "en"];
+    const defaultLocale = "vi";
+
+    const target =
+      supported.includes(browserLang) && browserLang !== defaultLocale
+        ? `/${browserLang}`
+        : "/";
+
+    if (target !== normalizedPath) {
+      window.location.replace(`/docs${target}`);
+    }
+  }, [history, location]);
+
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description={`${siteConfig.tagline}`}>
+    <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
--- src/theme/NavbarItem/LocaleDropdownNavbarItem/index.tsx
+++ src/theme/NavbarItem/LocaleDropdownNavbarItem/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import OriginalLocaleDropdownNavbarItem from "@theme-original/NavbarItem/LocaleDropdownNavbarItem";
 import { useLocation } from "@docusaurus/router";
+import { normalizePath } from "@site/src/utils/utils";
 
 export default function LocaleDropdownNavbarItemWrapper(props) {
   const location = useLocation();
+  const normalizedPath = normalizePath(location.pathname);
 
-  if (location.pathname.startsWith("/docs/en/developer") || location.pathname.startsWith("/docs/developer")) {
+  if (
+    normalizedPath.startsWith("/docs/en/developer") ||
+    normalizedPath.startsWith("/docs/developer") ||
+    normalizedPath === "/docs" ||
+    normalizedPath === "/docs/en"
+  ) {
     return null;
   }
 
